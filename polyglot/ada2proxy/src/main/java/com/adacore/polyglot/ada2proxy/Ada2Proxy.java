a2a6352456769508f6ac912fd441584446446128
Remi Segard <segard@adacore.com>
2025-06-17 17:34:09 +0200

    Ada2Proxy: Add CLI argument to choose binded units

--- polyglot/ada2proxy/src/main/java/com/adacore/polyglot/ada2proxy/Ada2Proxy.java
+++ polyglot/ada2proxy/src/main/java/com/adacore/polyglot/ada2proxy/Ada2Proxy.java
@@ -1,9 +1,12 @@
 package com.adacore.polyglot.ada2proxy;
 
 import com.adacore.polyglot.proxy.Proxy;
+import java.io.FileNotFoundException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -16,15 +19,27 @@ public class Ada2Proxy implements Callable<Integer> {
     Path gprfile;
 
     @Option(
-            names = {"output", "-o"},
+            names = {"-o", "--output"},
             description = "output path",
             required = true)
     Path outputPath;
 
+    @Option(
+            names = {"--units"},
+            description = "output path")
+    List<String> units = new ArrayList<>();
+
     @Override
     public Integer call() throws Exception {
         AdaScanner scanner = new AdaScanner();
-        scanner.scanProject(gprfile);
+
+        try {
+            scanner.scanProject(gprfile, units);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: %s".formatted(e.getMessage()));
+            return 1;
+        }
+
         Proxy p = scanner.getProxy();
         p.validate();
 
--- polyglot/proxy/src/main/java/com/adacore/polyglot/Scanner.java
+++ polyglot/proxy/src/main/java/com/adacore/polyglot/Scanner.java
@@ -1,15 +1,28 @@
 package com.adacore.polyglot;
 
 import com.adacore.polyglot.proxy.Proxy;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /** Base class to analyze projects and emit a Json proxy along with the proxy code */
 public abstract class Scanner {
 
-    /** Analyze a project and its files. */
-    public abstract void scanProject(Path projectFile);
+    /**
+     * Analyze a project and its files. If {@code units} is null or empty, all the files of the
+     * project will be analyzed.
+     *
+     * @throws FileNotFoundException
+     */
+    public abstract void scanProject(Path projectFile, List<String> units)
+            throws FileNotFoundException;
+
+    /** Analyze a project and all its files. */
+    public void scanProject(Path projectFile) throws FileNotFoundException {
+        scanProject(projectFile, null);
+    }
 
     /** Generate the code for the proxy. */
     public abstract void generate(Path path) throws IOException;
