c243d5d2ec1024824708c0f4c4cee72f4ad18efd
Remi Segard <segard@adacore.com>
2025-07-24 17:11:42 +0200

    Proxy2Cpp: support standard Ada exceptions

--- polyglot/runtimes/ada/src/polyglot_ada_exceptions.cpp
+++ polyglot/runtimes/ada/src/polyglot_ada_exceptions.cpp
@@ -26,4 +26,88 @@ AdaException::~AdaException() {
     polyglot__ada__exceptions__free_exception_occurence(this->_data);
 }
 
+ConstraintError::ConstraintError()
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence(
+              static_cast<int>(
+                  standard_exception_kind::CONSTRAINT_ERROR_KIND))) {}
+ConstraintError::ConstraintError(const strings::polyglot_string &what)
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence_message(
+              static_cast<int>(standard_exception_kind::CONSTRAINT_ERROR_KIND),
+              what.data()),
+          what) {}
+ConstraintError::ConstraintError(void *data)
+    : AdaException(data) {}
+ConstraintError::ConstraintError(void *data, const strings::polyglot_string &what)
+    : AdaException(data, what) {}
+
+ProgramError::ProgramError()
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence(
+              static_cast<int>(standard_exception_kind::PROGRAM_ERROR_KIND))) {}
+ProgramError::ProgramError(const strings::polyglot_string &what)
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence_message(
+              static_cast<int>(standard_exception_kind::PROGRAM_ERROR_KIND),
+              what.data()),
+          what) {}
+ProgramError::ProgramError(void *data)
+    : AdaException(data) {}
+ProgramError::ProgramError(void *data, const strings::polyglot_string &what)
+    : AdaException(data, what) {}
+
+StorageError::StorageError()
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence(
+              static_cast<int>(standard_exception_kind::STORAGE_ERROR_KIND))) {}
+StorageError::StorageError(const strings::polyglot_string &what)
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence_message(
+              static_cast<int>(standard_exception_kind::STORAGE_ERROR_KIND),
+              what.data()),
+          what) {}
+StorageError::StorageError(void *data)
+    : AdaException(data) {}
+StorageError::StorageError(void *data, const strings::polyglot_string &what)
+    : AdaException(data, what) {}
+
+TaskingError::TaskingError()
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence(
+              static_cast<int>(standard_exception_kind::TASKING_ERROR_KIND))) {}
+TaskingError::TaskingError(const strings::polyglot_string &what)
+    : AdaException(
+          polyglot__ada__exceptions__create_exception_occurence_message(
+              static_cast<int>(standard_exception_kind::TASKING_ERROR_KIND),
+              what.data()),
+          what) {}
+TaskingError::TaskingError(void *data)
+    : AdaException(data) {}
+TaskingError::TaskingError(void *data, const strings::polyglot_string &what)
+    : AdaException(data, what) {}
+
+void rethrow_standard_exception() {
+    kernel *k = polyglot_get_kernel();
+    void *data = k->exc_info.exception_data;
+    char *message =k->exc_info.message;
+    int kind = k->exc_info.exception_kind;
+    k->exc_info.exception_data = nullptr;
+    try {
+       switch (kind) {
+           case static_cast<int>(standard_exception_kind::CONSTRAINT_ERROR_KIND):
+               throw ConstraintError(data, message);
+           case static_cast<int>(standard_exception_kind::PROGRAM_ERROR_KIND):
+               throw ProgramError(data, message);
+           case static_cast<int>(standard_exception_kind::STORAGE_ERROR_KIND):
+               throw StorageError(data, message);
+           case static_cast<int>(standard_exception_kind::TASKING_ERROR_KIND):
+               throw TaskingError(data, message);
+       }
+    } catch (const std::exception&) {
+        k->exc_info.clear_exception(k);
+        throw;
+    }
+}
+
 } // namespace polyglot::ada::exceptions
--- polyglot/runtimes/ada/src/polyglot_ada_exceptions.h
+++ polyglot/runtimes/ada/src/polyglot_ada_exceptions.h
@@ -6,6 +6,13 @@
 
 namespace polyglot::ada::exceptions {
 
+enum class standard_exception_kind {
+    CONSTRAINT_ERROR_KIND = -4,
+    PROGRAM_ERROR_KIND = -3,
+    STORAGE_ERROR_KIND = -2,
+    TASKING_ERROR_KIND = -1
+};
+
 class AdaException : public polyglot::exceptions::polyglot_exception {
 public:
     AdaException(const strings::polyglot_string &what);
@@ -13,6 +20,40 @@ public:
     AdaException(void *data, const strings::polyglot_string &what);
     ~AdaException();
 };
+
+class ConstraintError : public AdaException {
+public:
+    ConstraintError();
+    ConstraintError(const strings::polyglot_string &what);
+    ConstraintError(void *data);
+    ConstraintError(void *data, const strings::polyglot_string &what);
+};
+
+class ProgramError : public AdaException {
+public:
+    ProgramError();
+    ProgramError(const strings::polyglot_string &what);
+    ProgramError(void *data);
+    ProgramError(void *data, const strings::polyglot_string &what);
+};
+
+class StorageError : public AdaException {
+public:
+    StorageError();
+    StorageError(const strings::polyglot_string &what);
+    StorageError(void *data);
+    StorageError(void *data, const strings::polyglot_string &what);
+};
+
+class TaskingError : public AdaException {
+public:
+    TaskingError();
+    TaskingError(const strings::polyglot_string &what);
+    TaskingError(void *data);
+    TaskingError(void *data, const strings::polyglot_string &what);
+};
+
+void rethrow_standard_exception();
 
 } // namespace polyglot::ada::exceptions
 
