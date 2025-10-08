cc1df4f90a5ef2b7cd3b4ed50231a38a3d9fda01
Remi Segard <segard@adacore.com>
2025-10-08 15:12:59 +0200

    Proxy2Cpp: Support operator overloading

---
+++ testsuite/tests/ada/operators/cpp/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "test.h"
+
+using namespace test;
+
+void print(const test::Pair &p) {
+    std::cout << "{" << p.get_left() << ", " << p.get_right() << "}\n";
+}
+
+int main() {
+    print(Pair(1, 2) + Pair(3, 4));
+    std::cout << +Pair(1, 3) << "\n";
+    print(Pair(3, 4) - Pair(1, 2));
+    std::cout << -Pair(1, 3) << "\n";
+    print(Pair(2, 3) * Pair(4, 5));
+    print(Pair(20, 15) / Pair(4, 5));
+    print(Pair(2, 2).operator_pow(Pair(4, 5)));
+    print(Pair(20, 15) % Pair(4, 5));
+    print(Pair(20, 15).operator_rem(Pair(4, 5)));
+    print(Pair(-15, 15));
+    print(Pair(20, 15).operator_concat(Pair(4, 5)));
+    std::cout << (Pair(1, 2) == Pair(1, 2)) << "\n";
+    std::cout << (Pair(1, 3) == Pair(1, 2)) << "\n";
+    std::cout << (Pair(1, 2) != Pair(1, 2)) << "\n";
+    std::cout << (Pair(1, 3) != Pair(1, 2)) << "\n";
+    print(Pair(1, 2) & Pair(3, 4));
+    print(Pair(1, 2) | Pair(3, 4));
+    print(Pair(-1, -1) ^ Pair(1, 2));
+    print(~Pair(~3, ~4));
+}
