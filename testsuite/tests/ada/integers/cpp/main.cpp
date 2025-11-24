8c4d78e045fa9328b40142226144a65c4dc00b71
Remi Segard <segard@adacore.com>
2025-11-24 12:41:42 +0100

    Update tests to avoid integer sizes conflicts

--- testsuite/tests/ada/integers/cpp/main.cpp
+++ testsuite/tests/ada/integers/cpp/main.cpp
@@ -1,41 +1,33 @@
-#include <cstdint>
+#include <cassert>
+#include <climits>
 #include <iostream>
 
 #include "ints.h"
 
-void p(uint8_t c) {
-    std::cout << "print char: " << (char) c << "\n";
-}
-
-void p(short s){
-    std::cout << "print short: " << s << "\n";
-}
-
-void p(int i){
-    std::cout << "print int: " << i << "\n";
-}
-
-void p(long l){
-    std::cout << "print long: " << l << "\n";
-}
-
-void p(unsigned int l){
-    std::cout << "print unsigned int: " << l << "\n";
+template <typename S, typename T>
+void minimum_size(T) {
+    assert(sizeof(S) <= sizeof(T));
 }
 
 int main() {
     std::cout << std::hex;
-    p(ints::f_char());
-    p(ints::f_short());
-    p(ints::f_int());
-    p(ints::f_long_int());
+    assert(ints::f_char() == 'P');
+    minimum_size<char>(ints::f_char());
+    assert(ints::f_short() == SHRT_MAX);
+    minimum_size<short>(ints::f_short());
+    assert(ints::f_int() == INT_MAX);
+    minimum_size<int>(ints::f_int());
+    assert(ints::f_long_int() == LONG_MAX);
+    minimum_size<long>(ints::f_long_int());
 
     std::cout << std::dec;
-    p(ints::f_my_int());
-    p(ints::f_my_new_short());
-    p(ints::f_my_long_int());
-    p(ints::f_my_small());
+    assert(ints::f_my_int() == (1 << 20));
+    assert(ints::f_my_new_short() == 1000);
+    assert(ints::f_my_long_int() == LONG_MIN);
+    assert(ints::f_my_small() == -256);
 
-    p(ints::f_positive());
-    p(ints::f_my_positive());
+    assert(ints::f_positive() == 1);
+    assert(ints::f_my_positive() == 2);
+    assert(typeid(ints::f_my_positive()) == typeid(unsigned)
+            || typeid(ints::f_my_positive()) == typeid(unsigned long));
 }
