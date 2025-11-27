d23e58b431ac206223de5e75a88b8a05bd7e5205
Remi Segard <segard@adacore.com>
2025-11-27 19:57:46 +0100

    Proxy2Cpp: support virtual types and functions

---
+++ testsuite/tests/ada/abstract_type/cpp/main.cpp
@@ -0,0 +1,25 @@
+#include "test.h"
+
+class CppChild : public test::Root {
+public:
+    CppChild() : test::Root(this) {}
+
+    void p2(int i) const override {
+        this->p1();
+        std::cout << "CppChild::p2 " << i << "\n";
+    }
+};
+
+int main() {
+    test::Child child(3);
+
+    child.p1();
+    child.p2(1);
+
+    CppChild cpp_child;
+
+    cpp_child.p1();
+    cpp_child.p2(2);
+
+    test::p2_root(cpp_child, 4);
+}
