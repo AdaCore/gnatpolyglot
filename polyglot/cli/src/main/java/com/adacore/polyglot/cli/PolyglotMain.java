accb5092b56e68241b3bfc3a137bf5cc76cf089f
Romain Beguet <beguet@adacore.com>
2026-02-03 12:28:11 +0100

    Make spotless consider CLI sources as well.

--- polyglot/cli/src/main/java/com/adacore/polyglot/cli/PolyglotMain.java
+++ polyglot/cli/src/main/java/com/adacore/polyglot/cli/PolyglotMain.java
@@ -1,40 +1,29 @@
 package com.adacore.polyglot.cli;
 
-import java.nio.file.Path;
-
 import com.adacore.polyglot.ada2proxy.Ada2Proxy;
-import com.adacore.polyglot.proxy.ProxyValidator;
 import com.adacore.polyglot.proxy.PolyglotSetup;
+import com.adacore.polyglot.proxy.ProxyValidator;
 import com.adacore.polyglot.proxy2cpp.Proxy2Cpp;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 @Command(
-    name = "polyglot",
-    subcommands = {
-        ProxyValidator.class,
-        Ada2Proxy.class,
-        Proxy2Cpp.class,
-        PolyglotSetup.class
-    },
-    version="0.1",
-    description="Polyglot driver."
-)
+        name = "polyglot",
+        subcommands = {ProxyValidator.class, Ada2Proxy.class, Proxy2Cpp.class, PolyglotSetup.class},
+        version = "0.1",
+        description = "Polyglot driver.")
 public class PolyglotMain {
     @Option(
-        names = {"-h", "--help"},
-        usageHelp = true,
-        description = "display usage and exit"
-    )
+            names = {"-h", "--help"},
+            usageHelp = true,
+            description = "display usage and exit")
     boolean helpRequested;
 
     @Option(
-        names = {"-v", "--version"},
-        versionHelp = true,
-        description = "display version and exit"
-    )
+            names = {"-v", "--version"},
+            versionHelp = true,
+            description = "display version and exit")
     boolean versionRequested;
 
     public static void main(String... args) {
