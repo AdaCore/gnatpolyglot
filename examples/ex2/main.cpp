42f87abb05c5714c6bf3ef3cb57c8fdd8f663155
Remi Segard <segard@adacore.com>
2026-03-19 15:43:34 +0100

    Update examples

--- examples/ex2/main.cpp
+++ examples/ex2/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 #include "2cpp/include/example.h"
-#include "polyglot_ada_arrays.h"
-#include "polyglot_ada_strings.h"
-#include "polyglot_ptr.h"
+#include "gnatpolyglot_ada_arrays.h"
+#include "gnatpolyglot_ada_strings.h"
+#include "gnatpolyglot_ptr.h"
 
 void escape_acc() {
-    polyglot::polyglot_ptr<example::P> acc(new example::P);
-    acc.set_owner(polyglot::memory_owner::LIBRARY);
+    gnatpolyglot::polyglot_ptr<example::P> acc(new example::P);
+    acc.set_owner(gnatpolyglot::memory_owner::LIBRARY);
     example::set_acc(acc);
     example::print();
     // Acc's owner is LIBRARY: The C++ object will be freed, but not the ada
@@ -17,7 +17,7 @@ void escape_acc() {
 int main() {
     escape_acc();
     example::print();
-    polyglot::polyglot_ptr<example::P> acc = example::get_acc();
+    gnatpolyglot::polyglot_ptr<example::P> acc = example::get_acc();
     example::unchecked_free(acc);
 
     example::set_acc(nullptr);
@@ -26,6 +26,6 @@ int main() {
     escape_acc();
     example::print();
     acc = example::get_acc();
-    acc.set_owner(polyglot::memory_owner::USER);
+    acc.set_owner(gnatpolyglot::memory_owner::USER);
     example::set_acc(nullptr);
 }
