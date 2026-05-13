7446e74ab242966a80a28208dc73d83d669caf74
Remi Segard <segard@adacore.com>
2026-05-13 17:41:41 +0200

    Ada2Proxy: fully block the signal handlers

--- gnatpolyglot/runtimes/ada/src/gnatpolyglot.ads
+++ gnatpolyglot/runtimes/ada/src/gnatpolyglot.ads
@@ -3,7 +3,7 @@
 --  SPDX-License-Identifier: Apache-2.0
 --
 
-pragma Unreserve_All_Interrupts;
+pragma Interrupts_System_By_Default;
 -- On GNU/Linux, the GNAT runtime installs a SIGSEGV signal handler, so turn
 -- memory issues into `Storage_Error` exceptions. This may be incompatible with
 -- the runtime of other languages, such as C++'s.
